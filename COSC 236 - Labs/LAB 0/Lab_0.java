fc60301e75688d1606d08fad808ba831f7774f50
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-01-30 17:17:43 -0500

    Lab 0 Completed

--- COSC 236 - Labs/LAB 0/Lab_0.java
+++ COSC 236 - Labs/LAB 0/Lab_0.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Lab_0 {
 
    /*
@@ -10,8 +8,9 @@ public class Lab_0 {
       Student ID:
       Year:
    */
-   static void Part_1(){
+   public static void Part_1(){
       System.out.println("\n1.");
+
       System.out.println("**My Profile**");
       System.out.println("Name:");
       System.out.println("Major");
@@ -25,14 +24,14 @@ public class Lab_0 {
       The Environmental path of Java is set to "C:\Program Files (x86)\Java\JDK\"
       This program's from the first lab
    */
-   static void Part_2(){
-      System.out.println("2.");
-      String java_path = "\"C:\\Program Files (x86)\\Java\\JDK\\\"";
+   public static void Part_2(){
+      System.out.println("\n2.");
+
+         String java_path = "\"C:\\Program Files (x86)\\Java\\JDK\\\""; //the java path string 
       
       System.out.println("Elon Musk just changed the name of \"Twitter\" to \"X\"");
-      System.out.println("The Environmental path of Java is set to" + java_path);
+      System.out.println("The Environmental path of Java is set to " + java_path);
       System.out.println("This program's from the first lab");
-
    }
    
    /* 
@@ -42,8 +41,8 @@ public class Lab_0 {
     *****
      ***
  */
-   static void Part_3(){
-      System.out.println("3.");
+   public static void Part_3(){
+      System.out.println("\n3.");
    
       System.out.println("  ***");
       System.out.println(" *****"); 
@@ -51,26 +50,24 @@ public class Lab_0 {
       System.out.println(" *****");
       System.out.println("  ***");
    }
-   
-   
-   static void Part_4(){
-      System.out.println("4.");
+    
+       /*\     4   
+     //***\\   2 
+    //*****\\  1
+    \\*****//* 1 
+     \\***//*  2 */
+//     \*/*    4*/ 
+   public static void Part_4(){
+      System.out.println("\n4.");
       
-      System.out.println("/*\\");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("");
-      System.out.println("");
-
-   
-   
-   
+      System.out.println("    " + "/*\\");
+      System.out.println("  " + "//***\\\\");
+      System.out.println(" " + "//*****\\\\");
+      System.out.println(" " + "\\\\*****//");
+      System.out.println("  " + "\\\\***//");
+      System.out.println("    " + "\\*/");
    }
-   
-   
+
    /*
       Mixing the dry ingredients 
       Add eggs 
@@ -79,8 +76,8 @@ public class Lab_0 {
       Set the timer
       Turn off the Oven
    */
-   static void Part_5(){
-      System.out.println("5.");
+   public static void Part_5(){
+      System.out.println("\n5.");
       
       System.out.println("Mixing the dry ingredients");
       System.out.println("Add eggs");
@@ -88,19 +85,41 @@ public class Lab_0 {
       System.out.println("Preheat the oven");
       System.out.println("Set the timer");
       System.out.println("Turn off the Oven");
-   
-
-   
    }
   
+   //part 6 is just part_4 x2
+   public static void Part_6(){
+      System.out.println("\n6.");
+
+      System.out.println("    " + "/*\\");
+      System.out.println("  " + "//***\\\\");
+      System.out.println(" " + "//*****\\\\");
+      System.out.println(" " + "\\\\*****//");
+      System.out.println("  " + "\\\\***//");
+      System.out.println("    " + "\\*/");
+
+      //same as above
+      System.out.println("    " + "/*\\");
+      System.out.println("  " + "//***\\\\");
+      System.out.println(" " + "//*****\\\\");
+      System.out.println(" " + "\\\\*****//");
+      System.out.println("  " + "\\\\***//");
+      System.out.println("    " + "\\*/");
+      
+   }
    public static void main(String[] args){
+
+      Part_1(); //finished
+
+      Part_2(); //finished
+
+      Part_3(); //finished
+
+      Part_4(); //finished
+     
+      Part_5(); //finished 
    
-     Part_1(); //finished
-     //Part_2(); //finished
-     //Part_3(); //finished
-     //Part_5(); //finished 
-   
-  
+      Part_6(); //finished
    }
 
 }
