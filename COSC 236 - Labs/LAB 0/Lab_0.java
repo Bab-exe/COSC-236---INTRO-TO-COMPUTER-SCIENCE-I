062520130ececde6c67966003297942d7688f8db
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-01-30 16:23:46 -0500

    Add files via upload
    Lab 0 released

---
+++ COSC 236 - Labs/LAB 0/Lab_0.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class Lab_0 {
+
+   /*
+      **My Profile**
+      Name:
+      Major
+      Track:
+      Student ID:
+      Year:
+   */
+   static void Part_1(){
+      System.out.println("\n1.");
+      System.out.println("**My Profile**");
+      System.out.println("Name:");
+      System.out.println("Major");
+      System.out.println("Track:");
+      System.out.println("Student ID:");
+      System.out.println("Year:");
+   }
+   
+   /*
+      Elon Musk just changed the name of "Twitter" to "X"
+      The Environmental path of Java is set to "C:\Program Files (x86)\Java\JDK\"
+      This program's from the first lab
+   */
+   static void Part_2(){
+      System.out.println("2.");
+      String java_path = "\"C:\\Program Files (x86)\\Java\\JDK\\\"";
+      
+      System.out.println("Elon Musk just changed the name of \"Twitter\" to \"X\"");
+      System.out.println("The Environmental path of Java is set to" + java_path);
+      System.out.println("This program's from the first lab");
+
+   }
+   
+   /* 
+     ***
+    *****
+   *******
+    *****
+     ***
+ */
+   static void Part_3(){
+      System.out.println("3.");
+   
+      System.out.println("  ***");
+      System.out.println(" *****"); 
+      System.out.println("*******"); 
+      System.out.println(" *****");
+      System.out.println("  ***");
+   }
+   
+   
+   static void Part_4(){
+      System.out.println("4.");
+      
+      System.out.println("/*\\");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("");
+
+   
+   
+   
+   }
+   
+   
+   /*
+      Mixing the dry ingredients 
+      Add eggs 
+      Add milk
+      Preheat the oven
+      Set the timer
+      Turn off the Oven
+   */
+   static void Part_5(){
+      System.out.println("5.");
+      
+      System.out.println("Mixing the dry ingredients");
+      System.out.println("Add eggs");
+      System.out.println("Add milk");
+      System.out.println("Preheat the oven");
+      System.out.println("Set the timer");
+      System.out.println("Turn off the Oven");
+   
+
+   
+   }
+  
+   public static void main(String[] args){
+   
+     Part_1(); //finished
+     //Part_2(); //finished
+     //Part_3(); //finished
+     //Part_5(); //finished 
+   
+  
+   }
+
+}
+
