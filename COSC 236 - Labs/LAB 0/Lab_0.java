0821117a778f6a3bc711a41928a06ffebd4b202b
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-02-04 21:35:58 -0500

    Lab 0 submitted

--- COSC 236 - Labs/LAB 0/Lab_0.java
+++ COSC 236 - Labs/LAB 0/Lab_0.java
@@ -9,8 +9,6 @@ public class Lab_0 {
       Year:
    */
    public static void Part_1(){
-      System.out.println("\n1.");
-
       System.out.println("**My Profile**");
       System.out.println("Name:");
       System.out.println("Major");
@@ -25,8 +23,6 @@ public class Lab_0 {
       This program's from the first lab
    */
    public static void Part_2(){
-      System.out.println("\n2.");
-
          String java_path = "\"C:\\Program Files (x86)\\Java\\JDK\\\""; //the java path string 
       
       System.out.println("Elon Musk just changed the name of \"Twitter\" to \"X\"");
@@ -42,8 +38,6 @@ public class Lab_0 {
      ***
  */
    public static void Part_3(){
-      System.out.println("\n3.");
-   
       System.out.println("  ***");
       System.out.println(" *****"); 
       System.out.println("*******"); 
@@ -57,9 +51,7 @@ public class Lab_0 {
     \\*****//* 1 
      \\***//*  2 */
 //     \*/*    4*/ 
-   public static void Part_4(){
-      System.out.println("\n4.");
-      
+   public static void Part_4(){      
       System.out.println("    " + "/*\\");
       System.out.println("  " + "//***\\\\");
       System.out.println(" " + "//*****\\\\");
@@ -77,8 +69,6 @@ public class Lab_0 {
       Turn off the Oven
    */
    public static void Part_5(){
-      System.out.println("\n5.");
-      
       System.out.println("Mixing the dry ingredients");
       System.out.println("Add eggs");
       System.out.println("Add milk");
@@ -89,38 +79,28 @@ public class Lab_0 {
   
    //part 6 is just part_4 x2
    public static void Part_6(){
-      System.out.println("\n6.");
-
-      System.out.println("    " + "/*\\");
-      System.out.println("  " + "//***\\\\");
-      System.out.println(" " + "//*****\\\\");
-      System.out.println(" " + "\\\\*****//");
-      System.out.println("  " + "\\\\***//");
-      System.out.println("    " + "\\*/");
-
-      //same as above
-      System.out.println("    " + "/*\\");
-      System.out.println("  " + "//***\\\\");
-      System.out.println(" " + "//*****\\\\");
-      System.out.println(" " + "\\\\*****//");
-      System.out.println("  " + "\\\\***//");
-      System.out.println("    " + "\\*/");
-      
+      Part_4();
+      Part_4();
    }
    public static void main(String[] args){
 
-      Part_1(); //finished
+      System.out.println("\n1.");
+         Part_1(); //finished
 
-      Part_2(); //finished
+      System.out.println("\n2.");
+         Part_2(); //finished
 
-      Part_3(); //finished
+      System.out.println("\n3.");
+         Part_3(); //finished
 
-      Part_4(); //finished
-     
-      Part_5(); //finished 
+      System.out.println("\n4.");
+         Part_4(); //finished
+
+      System.out.println("\n5.");
+         Part_5(); //finished 
    
-      Part_6(); //finished
+      System.out.println("\n6.");
+         Part_6(); //finished
    }
-
 }
 
