e51305d2b037571c3d846e45721e5a86cfa0d3bf
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-03-13 12:14:51 -0400

    Lab 6 assigned

---
+++ COSC 236 - Labs/LAB 6/Lab_6.java
@@ -0,0 +1,201 @@
+import java.util.*;
+
+public class Lab_6{
+
+        //1.Write a program that produces an inverted right triangle. The level/depth of the triangle depends on the console input. If the input is 5, the program prints the following-
+        public static void Triangle(Scanner input){
+
+            System.out.println("Level Of Inverted Triangle");
+                int level = input.nextInt();
+                final char symbol = '*';
+                
+            for (int i = 1; i<= level;i++){
+                for (int j = level;j>=i;j--){
+                    System.out.print(symbol);
+                }
+                System.out.println();
+            }
+        }
+
+
+
+        //todo
+        //2.Write a program that reads an input from the console and Prints a pyramid that has the same level as the input. For input 6 it prints the following pattern,
+        public static void Pyramid_2(Scanner input){
+            System.out.println("Input Level Of The Pyramid ");
+                int level = input.nextInt();
+
+                final char space = ' '; 
+
+
+                /* 5s  */
+            for (int i = 0;i<level; i++){
+
+                //spacing
+                for (int s = level;s>i;s--)
+                    System.out.print(space);
+
+                //numbers
+                for (int n = i+1;n<level;n++)
+                    System.out.print(i);
+
+            System.out.println();
+            }
+
+            //return output;
+        }
+  
+
+        //3.Print the following pattern based on an input from the console. For the input 5, print the following
+        public static void Pyramid_3(Scanner input){
+            final char symbol = '-';
+
+            String symbols = "";
+            System.out.println("Level Of Number pyramid (3):");
+                int level = input.nextInt();
+
+                for (int i = 1; i<=level;i++){
+
+                    //symbol 1 and making for symbol 2
+                    for (int s = level;s>=i;s--){
+                        symbols += symbol;
+                    }
+                        
+                    
+                    
+                    System.out.print(symbols);
+
+
+                    //number
+                    for (int n = 1; n<=i;n++){
+                        System.out.print(i);
+                    }
+                    
+
+
+                    //symbol 2 
+                    System.out.println(symbols);
+
+
+                    //clearing symbols for next loop
+                    symbols = "";
+                    
+                }
+            
+
+        }
+
+
+
+    
+        //4.Write a program that reads an integer input from the console and calculates and prints the sum of all the numbers starting from 1 to that number.
+        public static void SumTo(Scanner input){
+            System.out.println("Provide An Integer Input: ");
+            int number = input.nextInt();
+
+            int sum = 0;
+            
+
+           /* 1 = 1 
+            * 2 + 1 = 3 
+
+            1 + 2 + 3 = 6
+
+            */
+
+            for (int i = 1;i<=number;i++)
+                for (int j = i;j>=i;j--){
+                    sum += j;
+                    System.out.printf("Sum of the first %d numbers are %d\n",i,sum);
+                }
+                
+
+            
+        }
+
+
+        //todo
+        //5.Write a program that reads an integer input from the console and checks and prints all prime numbers from 1 to that number using nested for loops.
+        public static void Prime(Scanner input){
+            System.out.println("Enter An Integer Input ");
+                int number = input.nextInt();
+
+
+
+            System.out.printf("\nPrime Numbers From 1 to %d is :\n2\n",number);
+
+
+            
+            for (int i = 1;i <= number;i++)
+                for (int j = 2; j <= number;j++){
+                    if (j % i == 0){
+                        System.out.println(i);
+                        break;
+                    }
+                    
+
+                }
+
+                
+            
+            
+
+        }
+
+    
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Which Method Do You Want To Run");
+                int method = input.nextInt();
+
+
+            switch (method){
+                //question 1 ;done
+                case 1:
+                    System.out.println();
+                    Triangle(input);
+                break;
+
+
+                //question 2
+                case 2:
+                    System.out.println();
+                    Pyramid_2(input);
+                break;
+
+
+                //question 3 
+                case 3:
+                    System.out.println();
+                    Pyramid_3(input);
+                break;
+
+
+                //question 4 ;done
+                case 4:
+                    System.out.println();
+                    SumTo(input);
+                    
+                break;
+
+
+                //question 5 
+                case 5:
+                    System.out.println();
+                    Prime(input);
+                break;
+
+
+                default:
+                    System.out.printf("\n%d is Not An Option",method);
+                break;
+            };
+
+
+
+
+
+
+        }
+}
